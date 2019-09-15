b977668b004d07ed80763127471f540c9da37f93
Amstrom <sahandilshansg@gmail.com>
2019-09-16 00:48:38 +0530

    add grpc-sink new implementation with testcases

--- README.md
+++ README.md
@@ -23,7 +23,7 @@ Latest API Docs is <a target="_blank" href="https://siddhi-io.github.io/siddhi-i
 
 ## Features
 ##### Default Service Protobuf Definition
-```proto
+```io.siddhi.extension.io.grpc.proto
 syntax = "proto3";
 
 option java_multiple_files = true;
@@ -31,7 +31,7 @@ option java_package = "org.wso2.grpc";
 
 package org.wso2.grpc.eventservice;
 
-import "google/protobuf/empty.proto";
+import "google/protobuf/empty.io.siddhi.extension.io.grpc.proto";
 
 service EventService {
     rpc process(Event) returns (Event) {}
@@ -46,7 +46,7 @@ message Event {
 * <a target="_blank" href="https://siddhi-io.github.io/siddhi-io-grpc/api/1.0.0/#grpc-call-sink">grpc-call</a> *<a target="_blank" href="https://siddhi.io/en/v5.0/docs/query-guide/#sink">(Sink)</a>*<br><div style="padding-left: 1em;"><p><br>This extension publishes event data encoded into GRPC Classes as defined in the user input jar. This extension has a default gRPC service classes jar added. The default service is called "EventService". This grpc-call sink is used for scenarios where we send a request out and expect a response back. In default mode this will use EventService process method. grpc-call-response source is used to receive the responses. A unique <code>sink.id</code> is used to correlate between the sink and its corresponding source.</p></div>
 * <a target="_blank" href="https://siddhi-io.github.io/siddhi-io-grpc/api/1.0.0/#grpc-service-response-sink">grpc-service-response</a> *<a target="_blank" href="https://siddhi.io/en/v5.0/docs/query-guide/#sink">(Sink)</a>*<br><div style="padding-left: 1em;"><p><br>This extension is used to send responses back to a gRPC client after receiving requests through grpc-service source. This correlates with the particular source using a unique <code>source.id</code></p></div>
 * <a target="_blank" href="https://siddhi-io.github.io/siddhi-io-grpc/api/1.0.0/#grpc-source">grpc</a> *<a target="_blank" href="https://siddhi.io/en/v5.0/docs/query-guide/#source">(Source)</a>*<br><div style="padding-left: 1em;"><p><br>This extension starts a grpc server during initialization time. The server listens to requests from grpc stubs. This source has a default mode of operation and custom user defined grpc service mode. In the default mode this source will use EventService consume method. This method will receive requests and injects them into stream through a mapper.</p></div>
-* <a target="_blank" href="https://siddhi-io.github.io/siddhi-io-grpc/api/1.0.0/#grpc-service-source">grpc-service</a> *<a target="_blank" href="https://siddhi.io/en/v5.0/docs/query-guide/#source">(Source)</a>*<br><div style="padding-left: 1em;"><p><br>This extension implements a grpc server for receiving and responding to requests. During initialization time a grpc server is started on the user specified port exposing the required service as given in the url. This source also has a default mode and a user defined grpc service mode. In the default mode this will use the EventService process method. This accepts grpc message class Event as defined in the EventService proto. This uses GrpcServiceResponse sink to send reponses back in the same Event message format.</p></div>
+* <a target="_blank" href="https://siddhi-io.github.io/siddhi-io-grpc/api/1.0.0/#grpc-service-source">grpc-service</a> *<a target="_blank" href="https://siddhi.io/en/v5.0/docs/query-guide/#source">(Source)</a>*<br><div style="padding-left: 1em;"><p><br>This extension implements a grpc server for receiving and responding to requests. During initialization time a grpc server is started on the user specified port exposing the required service as given in the url. This source also has a default mode and a user defined grpc service mode. In the default mode this will use the EventService process method. This accepts grpc message class Event as defined in the EventService io.siddhi.extension.io.grpc.proto. This uses GrpcServiceResponse sink to send reponses back in the same Event message format.</p></div>
 * <a target="_blank" href="https://siddhi-io.github.io/siddhi-io-grpc/api/1.0.0/#grpc-call-response-source">grpc-call-response</a> *<a target="_blank" href="https://siddhi.io/en/v5.0/docs/query-guide/#source">(Source)</a>*<br><div style="padding-left: 1em;"><p><br>This grpc source receives responses received from gRPC server for requests sent from a gRPC sink. The source will receive responses for sink with the same <code>sink.id</code>. For example if you have a gRPC sink with <code>sink.id</code> 15 then we need to set the <code>sink.id</code> as 15 in the source to receives responses. Sinks and sources have 1:1 mapping.</p></div>
 
 
---
+++ component/src/test/java/io/siddhi/extension/io/grpc/sink/GrpcSinkGenericTestCases.java
@@ -0,0 +1,264 @@
+/*
+ * Copyright (c)  2019, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package io.siddhi.extension.io.grpc.sink;
+
+import io.siddhi.core.SiddhiAppRuntime;
+import io.siddhi.core.SiddhiManager;
+import io.siddhi.core.stream.input.InputHandler;
+import io.siddhi.extension.io.grpc.utils.GenericTestServer;
+import io.siddhi.extension.io.grpc.utils.TestAppender;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.log4j.spi.LoggingEvent;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class GrpcSinkGenericTestCases {
+    private static final Logger log = Logger.getLogger(GrpcSinkTestCase.class.getName());
+    private GenericTestServer server = new GenericTestServer();
+    private String packageName = "io.siddhi.extension.io.grpc.proto";
+
+    @BeforeTest
+    public void init() throws IOException {
+        server.start();
+    }
+
+    @AfterTest
+    public void stop() throws InterruptedException {
+        server.stop();
+    }
+
+    @Test
+    public void testCase01() throws Exception {
+        log.info("Test case to call send");
+        log.setLevel(Level.DEBUG);
+        final TestAppender appender = new TestAppender();
+        final Logger rootLogger = Logger.getRootLogger();
+        rootLogger.setLevel(Level.DEBUG);
+        rootLogger.addAppender(appender);
+        SiddhiManager siddhiManager = new SiddhiManager();
+
+        String inStreamDefinition = ""
+                + "@sink(type='grpc', publisher.url = 'grpc://localhost:8888/"+ packageName + ".MyService/send', " +
+                "@map(type='protobuf')) " +
+                "define stream FooStream (stringValue string, intValue int,longValue long,booleanValue bool," +
+                "floatValue float,doubleValue double);";
+
+        SiddhiAppRuntime siddhiAppRuntime = siddhiManager.createSiddhiAppRuntime(inStreamDefinition);
+        InputHandler fooStream = siddhiAppRuntime.getInputHandler("FooStream");
+
+        siddhiAppRuntime.start();
+        fooStream.send(new Object[]{"Test 01", 60, 10000L, true, 522.7586f, 34.5668});
+        Thread.sleep(1000);
+        siddhiAppRuntime.shutdown();
+
+        final List<LoggingEvent> log = appender.getLog();
+        List<String> logMessages = new ArrayList<>();
+        for (LoggingEvent logEvent : log) {
+            String message = String.valueOf(logEvent.getMessage());
+            logMessages.add(message);
+        }
+        Assert.assertTrue(logMessages.contains("Server hits with request :\n" +
+                "stringValue: \"Test 01\"\n" +
+                "intValue: 60\n" +
+                "longValue: 10000\n" +
+                "booleanValue: true\n" +
+                "floatValue: 522.7586\n" +
+                "doubleValue: 34.5668\n"));
+    }
+
+    @Test
+    public void testWithMappingAttributes() throws Exception {
+        log.info("Test case to call send");
+        log.setLevel(Level.DEBUG);
+        SiddhiManager siddhiManager = new SiddhiManager();
+        final TestAppender appender = new TestAppender();
+        final Logger rootLogger = Logger.getRootLogger();
+        rootLogger.setLevel(Level.DEBUG);
+        rootLogger.addAppender(appender);
+
+        String inStreamDefinition = ""
+                + "@sink(type='grpc', " +
+                "publisher.url = 'grpc://localhost:8888/" + packageName + ".MyService/send', " +
+                "@map(type='protobuf', " +
+                "@payload(stringValue='a',longValue='b',intValue='c',booleanValue='d',floatValue = 'e', doubleValue =" +
+                " 'f'))) " +
+                "define stream FooStream (a string, b long, c int,d bool,e float,f double);";
+
+        SiddhiAppRuntime siddhiAppRuntime = siddhiManager.createSiddhiAppRuntime(inStreamDefinition);
+        InputHandler fooStream = siddhiAppRuntime.getInputHandler("FooStream");
+        siddhiAppRuntime.start();
+        fooStream.send(new Object[]{"Test 01", 10000L, 60, true, 522.7586f, 34.5668});
+        Thread.sleep(1000);
+        fooStream.send(new Object[]{"Test 02", 10000L, 60, false, 768.987f, 34.5668});
+        Thread.sleep(1000);
+        siddhiAppRuntime.shutdown();
+
+        final List<LoggingEvent> log = appender.getLog();
+        List<String> logMessages = new ArrayList<>();
+        for (LoggingEvent logEvent : log) {
+            String message = String.valueOf(logEvent.getMessage());
+            logMessages.add(message);
+        }
+        Assert.assertTrue(logMessages.contains("Server hits with request :\n" +
+                "stringValue: \"Test 02\"\n" +
+                "intValue: 60\n" +
+                "longValue: 10000\n" +
+                "floatValue: 768.987\n" +
+                "doubleValue: 34.5668\n"));
+    }
+
+    @Test
+    public void testWithMapObject() throws Exception {
+        log.info("Test case to call send");
+        log.setLevel(Level.DEBUG);
+        SiddhiManager siddhiManager = new SiddhiManager();
+
+        String inStreamDefinition = ""
+                + "@sink(type='grpc', " +
+                "publisher.url = 'grpc://localhost:8888/" + packageName + ".MyService/testMap', " +
+                "@map(type='protobuf')) " +
+                "define stream FooStream (stringValue string, intValue int,map object);";
+
+        SiddhiAppRuntime siddhiAppRuntime = siddhiManager.createSiddhiAppRuntime(inStreamDefinition);
+        InputHandler fooStream = siddhiAppRuntime.getInputHandler("FooStream");
+        siddhiAppRuntime.start();
+        Map<String, String> mapObject = new HashMap<>();
+        mapObject.put("Key 01", "Value 01");
+        mapObject.put("Key 02", "Value 02");
+        fooStream.send(new Object[]{"Test 01", 60, mapObject});
+
+        Thread.sleep(1000);
+        siddhiAppRuntime.shutdown();
+
+    }
+
+    @Test
+    public void testWithMetadata() throws Exception {
+        log.info("Test case to call send");
+        log.setLevel(Level.DEBUG);
+        SiddhiManager siddhiManager = new SiddhiManager();
+
+        String inStreamDefinition = ""
+                + "@sink(type='grpc', " +
+                "publisher.url = 'grpc://localhost:8888/org.wso2.grpc.test.MyService/send', " +
+                "metadata='{{metadata}}', " +
+                "@map(type='protobuf', " +
+                "@payload(stringValue='a',longValue='b',intValue='c',booleanValue='d',floatValue = 'e', doubleValue =" +
+                " 'f'))) " +
+                "define stream FooStream (a string, b long, c int,d bool,e float,f double, metadata string);";
+
+        SiddhiAppRuntime siddhiAppRuntime = siddhiManager.createSiddhiAppRuntime(inStreamDefinition);
+        InputHandler fooStream = siddhiAppRuntime.getInputHandler("FooStream");
+        siddhiAppRuntime.start();
+        fooStream.send(new Object[]{"Test 01", 10000L, 60, true, 522.7586f, 34.5668, "'Name:Sahan','Age:21'," +
+                "'Content-Type:json'"});
+        Thread.sleep(1000);
+        fooStream.send(new Object[]{"Test 02", 10000L, 60, false, 768.987f, 34.5668, "'Name:Sahan','Age:21'," +
+                "'Content-Type:json'"});
+        Thread.sleep(1000);
+        siddhiAppRuntime.shutdown();
+
+    }
+
+
+    @Test
+    public void testCase_SendAsStream() throws Exception {
+        log.info("Test case to call send");
+        log.setLevel(Level.DEBUG);
+        SiddhiManager siddhiManager = new SiddhiManager();
+        final TestAppender appender = new TestAppender();
+        final Logger rootLogger = Logger.getRootLogger();
+        rootLogger.setLevel(Level.DEBUG);
+        rootLogger.addAppender(appender);
+
+        String inStreamDefinition = ""
+                + "@sink(type='grpc', publisher.url = 'grpc://localhost:8888/" + packageName +
+                ".StreamService/clientStream', " +
+                "@map(type='protobuf')) " +
+                "define stream FooStream (stringValue string, intValue int,longValue long,booleanValue bool," +
+                "floatValue float,doubleValue double);";
+
+        SiddhiAppRuntime siddhiAppRuntime = siddhiManager.createSiddhiAppRuntime(inStreamDefinition);
+        InputHandler fooStream = siddhiAppRuntime.getInputHandler("FooStream");
+
+        siddhiAppRuntime.start();
+        for (int i = 1; i <= 50; i++) {
+            fooStream.send(new Object[]{"Test " + i, i, i * 1000L, true, 10.456f * i, 34.5668 * i});
+        }
+        Thread.sleep(1000);
+        siddhiAppRuntime.shutdown();
+
+        final List<LoggingEvent> log = appender.getLog();
+        List<String> logMessages = new ArrayList<>();
+        for (LoggingEvent logEvent : log) {
+            String message = String.valueOf(logEvent.getMessage());
+            logMessages.add(message);
+        }
+        Assert.assertTrue(logMessages.contains("Done Streaming"));
+    }
+
+    @Test
+    public void testCase0_SendAsStream_SendMap() throws Exception {
+        log.info("Test case to call send");
+        log.setLevel(Level.DEBUG);
+        SiddhiManager siddhiManager = new SiddhiManager();
+        final TestAppender appender = new TestAppender();
+        final Logger rootLogger = Logger.getRootLogger();
+        rootLogger.setLevel(Level.DEBUG);
+        rootLogger.addAppender(appender);
+
+        String inStreamDefinition = ""
+                + "@sink(type='grpc', publisher.url = 'grpc://localhost:8888/" + packageName +
+                ".StreamService/clientStreamWithMap', " +
+                "@map(type='protobuf')) " +
+                "define stream FooStream (stringValue string, intValue int, map object);";
+
+        SiddhiAppRuntime siddhiAppRuntime = siddhiManager.createSiddhiAppRuntime(inStreamDefinition);
+        InputHandler fooStream = siddhiAppRuntime.getInputHandler("FooStream");
+
+        siddhiAppRuntime.start();
+        Map<String, String> map = new HashMap<>();
+
+        for (int i = 1; i <= 20; i++) {
+            map.put("Key " + i, "Value " + i);
+            fooStream.send(new Object[]{"Test " + i, i, map});
+            Thread.sleep(100);
+            map.clear();
+        }
+        siddhiAppRuntime.shutdown();
+        final List<LoggingEvent> log = appender.getLog();
+        List<String> logMessages = new ArrayList<>();
+        for (LoggingEvent logEvent : log) {
+            String message = String.valueOf(logEvent.getMessage());
+            logMessages.add(message);
+        }
+        Assert.assertTrue(logMessages.contains("Done Streaming"));
+
+    }
+
+
+}
